38ce3496b4b678d65902debe9d47ce9c05006fdd
allinthemind66 <r.nilon92@gmail.com>
2018-10-10 21:39:27 -0400

    re-arrange code in auth controller create method

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -4,8 +4,9 @@ class Api::V1::AuthController < ApplicationController
   skip_before_action :authorized, only: [:create, :show]
 
   def create
-    # byebug
+    byebug
     user = User.find_by(username: user_login_params[:username])
+    user.authenticate(user_login_params[:password])
     if user && user.authenticate(user_login_params[:password])
       token = encode_token({user_id: user.id})
       render json: { id: user.id, username: user.username, jwt: token }
