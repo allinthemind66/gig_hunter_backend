5808308514f7735a406b07639cae6c8e47995701
allinthemind66 <r.nilon92@gmail.com>
2018-10-11 11:37:31 -0400

    add more info to friend requests controller for debugging

--- app/controllers/api/v1/friend_requests_controller.rb
+++ app/controllers/api/v1/friend_requests_controller.rb
@@ -17,6 +17,8 @@ class Api::V1::FriendRequestsController < ApplicationController
   def create
     puts 'this is the current user'
     puts current_user
+    puts 'these are the params'
+    puts params
     @friendRequest = FriendRequest.find_or_create_by(friend_id: params[:friendId], user_id: current_user.id)
     render json: @friendRequest
   end
